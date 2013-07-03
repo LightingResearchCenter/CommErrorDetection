7d2a22ab80e4a98ad1e0cc82d1fc8ebc0006f012
Geoffrey Jones <jonesg5@rpi.edu>
2013-07-03 14:14:36 -0400

    Initial Commit

---
+++ checkFile.m
@@ -0,0 +1,25 @@
+function [ deviceID, comErrors, resets ] = checkFile( fileName )
+%CHECKFILE checks a Daysimeter file for communication errors and resets
+%   Returns the device ID and a summary of communication errors and resets
+
+%% Read file
+
+
+%% Find device ID
+
+
+%% Find communication errors
+
+
+%% Summarize communication errors
+
+
+%% Find resets
+
+
+%% Summarize resets
+
+
+
+end
+
